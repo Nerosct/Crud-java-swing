9074197794243eeaa459b0e826995fcbc1d3c4f4
neros <noslinedsotnas72@gmail.com>
2026-09-13 16:36:11 -0300

    docs: comentário

--- src/main/java/com/sisaudcom/crud/javaswing/connection/Conexao.java
+++ src/main/java/com/sisaudcom/crud/javaswing/connection/Conexao.java
@@ -6,6 +6,9 @@ import java.sql.SQLException;
 
 public class Conexao {
 
+    
+    // Não é recomendado colocar as variáveis de conexão e senhas no código, fiz dessa forma para facilitar a avaliação
+    // num outro cenário, criaria variáveis de ambiente
     private static final String URL = "jdbc:postgresql://localhost:5432/app-db";
     private static final String USUARIO = "postgres";
     private static final String SENHA = "postgres";
